b5c3d2d82273189bdcf075d54f78eabd22c759f1
João Vitor de Sousa Secco <90794101+secc0@users.noreply.github.com>
2023-03-10 15:36:03 -0300

    Update nikemonitor.py

--- nikemonitor.py
+++ nikemonitor.py
@@ -1,64 +1,27 @@
 import requests
 import json
-import time
+from discord_webhook import DiscordWebhook, DiscordEmbed
 
+url = "https://apigateway.nike.com.br/nike-bff/search/nav/genero/masculino/idade/adulto/tipodeproduto/calcados"
 
-INSTOCK = []
+querystring = {"page": "2", "sorting": "relevance", "resultsPerPage": "40",
+               "scoringProfile": "scoreByRanking", "multiFilters": "false"}
 
-WEBHOOK = ''
-
+payload = ""
 headers = {
-    
-    'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Mobile Safari/537.36',
+    "user-agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Mobile Safari/537.36",
 }
 
-url = 'https://apigateway.nike.com.br/nike-bff/search/nav/genero/masculino/idade/adulto/tipodeproduto/calcados?page=2&sorting=relevance&resultsPerPage=40&scoringProfile=scoreByRanking&multiFilters=false'
-r = requests.get(url, headers=headers)
+response = requests.request(
+    "GET", url, data=payload, headers=headers, params=querystring)
 
+data = json.loads(response.text)
+products = data['products']
 
-def scrape_site():
-    html = requests.get(url=url)
-    output = json.loads(html.text)
-    return output['products']
-
-
-def discord_webhook(name, price, url):
-        data = {
-            'embeds': [{
-                'name': name,
-                'fields': [
-                    {'name': 'Price', 'value': str(price)},
-                    {'name': 'url', 'value': url}
-                ]
-            }]
-        }
-        result = requests.post(WEBHOOK, data=json.dumps(data), headers={'Content-type':'aplication/json'})
-        print(result.status_code)
-
-
-def comparison(item, start):
-    if(item['id'] in INSTOCK) and (item['inStock'] == False):
-        INSTOCK.remove(item['id'])
-
-    elif(item['id'] not in INSTOCK) and (item['inStock'] == True):
-        INSTOCK.append(item['id'])
-        if start== 0:
-            discord_webhook(
-                name=item['name'],
-                price=item['price'],
-                url=item['url']
-            )
-            print()
-
-
-
-""" start = 0
-while True:
-    products = scrape_site()
-    for product in products:
-        comparison(product, start)
-    
-
-    start = 0
-    time.sleep(5) """
-    
+for product in products:
+    message = f"{product['name']} - R$ {product['price']}"
+    webhook = DiscordWebhook(
+        url='urldochat',
+        content=message
+    )
+    response = webhook.execute()
